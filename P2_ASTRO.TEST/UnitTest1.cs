c64420618fe8d0b88686d8bcb5b908cc3710fe43
JustinTheyskens <justin.theyskens@stonybrook.edu>
2025-01-09 16:39:32 -0500

    finished create user and create review unit tests - attempt 2

--- P2_ASTRO.TEST/UnitTest1.cs
+++ P2_ASTRO.TEST/UnitTest1.cs
@@ -1,8 +1,10 @@
 using Moq;
 using P2_ASTRO.API.Model;
 using P2_ASTRO.API.Repository;
-//using P2_ASTRO.API.Service;
+using P2_ASTRO.API.Service;
 using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using P2_ASTRO.API.Util;
 
 namespace P2_ASTRO.TEST;
 
@@ -13,7 +15,9 @@ public class UnitTest1
     {
         Mock<IUserRepository> mockRepo = new();
         Mock<IMapper> mockMapper = new ();
-        //UserService UserService = new (mockRepo.Object, mockMapper.Object);
+        //Mock<Utility> mockUtil = new();
+        API.Util.Utility util = new();
+        UserService userService = new (mockRepo.Object, util); //, mockMapper.Object);
 
         List<User> userList =
         [
@@ -23,27 +27,44 @@ public class UnitTest1
            
         ];
 
+        // Act
         var newUser =  new User(){Username = "JTheyskens", Password = "password3"};
 
-        // mockRepo.Setup(repo => repo.CreateNewUser(It.IsAny<User>()))
-        //     .Callback((User u) => orderList.Add(u))
-        //     .Returns(newUser);
+         mockRepo.Setup(repo => repo.CreateNewUser(It.IsAny<User>()))
+             .Callback((User u) => userList.Add(u))
+             .Returns(newUser);
+
+        // Assert
+        Assert.Contains(newUser, userList);
+        mockRepo.Verify(x => x.CreateNewUser(It.IsAny<User>()), Times.Once());
             
     }
 
     [Fact]
     public void CreateReviewTest()
     {
-        Mock<IUserRepository> mockRepo = new();
+        Mock<IReviewRepository> mockRepo = new();
         Mock<IMapper> mockMapper = new ();
-        //UserService UserService = new (mockRepo.Object, mockMapper.Object);
-        List<Review> reviews=
+        API.Util.Utility util = new();
+        ReviewService reviewService = new (mockRepo.Object, util); //mockMapper.Object);
+        List<Review> reviewList =
         [
             new Review(){Comment = "Test 1"},
             new Review(){Comment = "Test 2"},
             new Review(){Comment = "Test 3"},
 
         ];
+
+        // Act
+        var newRev = new Review(){Comment = "this is my comment!"};
+
+        mockRepo.Setup(repo => repo.CreateNewReview(It.IsAny<Review>()))
+            .Callback((Review r) => reviewList.Add(r))
+            .Returns(newRev);
+
+        // Assert
+        Assert.Contains(newRev, reviewList);
+        mockRepo.Verify(x => x.CreateNewReview(It.IsAny<Review>()), Times.Once());
 
     }
 }
