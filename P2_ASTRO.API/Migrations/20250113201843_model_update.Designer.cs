0496c020a229ea49b57b47f4ba4eb62aa028ddc3
Praween Pongpat <praween.pongpat@gmail.com>
2025-01-13 16:06:11 -0500

    add dependency inj, migration

--- P2_ASTRO.API/Migrations/20250113201843_model_update.Designer.cs
+++ P2_ASTRO.API/Migrations/20250113201843_model_update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using P2_ASTRO.API.Data;
 
@@ -11,9 +12,11 @@ using P2_ASTRO.API.Data;
 namespace P2_ASTRO.API.Migrations
 {
     [DbContext(typeof(AstroContext))]
-    partial class AstroContextModelSnapshot : ModelSnapshot
+    [Migration("20250113201843_model_update")]
+    partial class model_update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- P2_ASTRO.API/Program.cs
+++ P2_ASTRO.API/Program.cs
@@ -1,21 +1,40 @@
 using Microsoft.EntityFrameworkCore;
 using P2_ASTRO.API.Data;
 using P2_ASTRO.API.Repository;
-using P2_ASTRO.API;
-
+using P2_ASTRO.API.Service;
+using P2_ASTRO.API.Util;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-// Add cbcontext and connect it to connection string
-builder.Services.AddDbContext<AstroContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AstroDB")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add cbcontext and connect it to connection string
+builder.Services.AddDbContext<AstroContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AstroDB")));
+
+//Add service dependencies
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddScoped<IPODService, PODService>();
+
+//Add repo dependencies
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IPODRepository, PODRepository>();
+
+//Use singleton for utilies
+builder.Services.AddSingleton<Utility>(); 
+
+//Add controllers
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 var app = builder.Build();
 
@@ -27,6 +46,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
+app.MapControllers();
 app.Run();
